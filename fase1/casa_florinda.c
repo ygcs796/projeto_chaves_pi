bc985f486fd06c1f24c6e8b957a845b99805cf78
ygcs796 <ygcs@cin.ufpe.br>
2025-11-24 18:42:11 -0300

    corrigi colocar uma caixa de diálogos provisória quando o chaves colide com a porta da dona florinda, mas ainda é necessário conectar essa colisão com o aparecimento da casa da dona florinda

--- fase1/casa_florinda.c
+++ fase1/casa_florinda.c
@@ -1,11 +1,11 @@
-#include <stdio.h>
-#include "raylib.h"
-#include "fase1.h"
+// #include <stdio.h>
+// #include "raylib.h"
+// #include "fase1.h"
 
-void carregar_casa_florinda() {
+// void carregar_casa_florinda() {
 
-    Image imagem_casa_florinda = LoadImage("./imagens/casaFlorinda.png");
+//     Image imagem_casa_florinda = LoadImage("./imagens/casaFlorinda.png");
 
-    ImageResize(imagem_casa_florinda, ) // falta completar aqui
+//     ImageResize(imagem_casa_florinda, ) // falta completar aqui
 
-}
+// }
